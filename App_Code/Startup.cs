db0d0a168dfc4f7becf9c40c86795f1c11fa3cea
hsarode-compumatrice <hsarode-compumatrice@github.com>
2016-02-09 11:47:00 +0530

    first CI

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Continuous_integration.Startup))]
+namespace Continuous_integration
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
